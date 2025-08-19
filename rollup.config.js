61e521bac0e2f4540f5e96abdedc062ce8461596
이찬우 <leechanwoo@MacBook-Pro-M3-1T.local>
2025-08-19 13:05:23 +0900

    optimized build setup

--- rollup.config.js
+++ rollup.config.js
@@ -10,49 +10,51 @@ export default [
   {
     input: 'src/index.tsx',
     output: {
-      file: pkg.module,           // dist/index.esm.js
+      file: pkg.module,           // dist/index.mjs
       format: 'es',
       sourcemap: true
     },
     plugins: [
       typescript({
         tsconfig: './tsconfig.json',
-        clean: true
+        clean: true,
+        useTsconfigDeclarationDir: true
       }),
-      resolve(),
+      resolve({ browser: true, preferBuiltins: false }),
       babel({
         babelHelpers: 'bundled',
-        presets: ['@babel/preset-react', '@babel/preset-typescript'],
+        presets: ['@babel/preset-react'],
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
         exclude: 'node_modules/**'
       }),
       commonjs(),
     ],
-    external: ['react', 'react-dom', 'react-plaid-link']
+    external: ['react', 'react-dom']
   },
   // CommonJS 빌드
   {
     input: 'src/index.tsx',
     output: {
-      file: pkg.main,             // dist/index.cjs.js
+      file: pkg.main,             // dist/index.cjs
       format: 'cjs',
       sourcemap: true
     },
     plugins: [
       typescript({
         tsconfig: './tsconfig.json',
-        clean: true
+        clean: true,
+        useTsconfigDeclarationDir: true
       }),
-      resolve(),
+      resolve({ browser: true, preferBuiltins: false }),
       babel({
         babelHelpers: 'bundled',
-        presets: ['@babel/preset-react', '@babel/preset-typescript'],
+        presets: ['@babel/preset-react'],
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
         exclude: 'node_modules/**'
       }),
       commonjs(),
       terser()                    // 최소화(옵션)
     ],
-    external: ['react', 'react-dom', 'react-plaid-link']
+    external: ['react', 'react-dom']
   }
 ];
